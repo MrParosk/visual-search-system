f4b8201cb170905bad2cb8b831ec90b0a1f4a578
MrParosk <erik.parlstrand@gmail.com>
2023-10-22 19:46:28 +0200

    fixed docker paths

--- nearest-neighbor-index-creation/build_run.sh
+++ nearest-neighbor-index-creation/build_run.sh
@@ -7,7 +7,7 @@ docker build -t nearest_neighbor_index_container .
 docker run \
     --gpus all \
     --shm-size=2G \
-    -v $(pwd):/home/user/ \
+    -v $(pwd):/home/user/workdir/ \
     -v $(pwd)/../data:/home/user/data \
     -v $(pwd)/../artifact:/home/user/artifact \
     -it \
